0b59b8c430f876ac83b47bb651dc49135dd17033
Ryan <ryannitz@amazon.com>
2018-08-27 17:49:20 -0400

    Dev

--- test.py
+++ test.py
@@ -6,24 +6,21 @@ def main():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=FORMAT)
     logger = logging.getLogger(__name__)
 
-    """ A very simple test that showcases generating random data and then writing it to
-    an S3 bucket. Your container must have permission to write to the S3 bucket that
-    is provided as an environment variable. """
-    logger.info('Hello world - batch')
+    try:
 
-    logger.info('Generate test data locally')
-    with open('output_file', 'wb') as fout:
-        fout.write(os.urandom(49152))
-        fout.close()
+        sqs = boto3.resource('sqs')
+        queue = sqs.get_queue_by_name(QueueName=os.environ['SQS_QUEUE'])
 
-    logger.info('Push test data to S3')
-    s3_client = boto3.resource('s3')
+        while True:
+            for message in queue.receive_messages():
+                logger.info('message received')
+                message.delete()
 
-    with open('output_file', 'rb') as data:
-        s3_client.Bucket(os.environ['S3_BUCKET_NAME']).put_object(Key='output_file', Body=data)
-        data.close()
+        # TODO: Catch the signal to stop and exit cleanly
 
-    logger.info('Done')
+    except KeyError:
+        logger.error('Please set the environment variable SQS_QUEUE')
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
